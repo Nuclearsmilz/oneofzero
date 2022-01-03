728edc84af520220684a70e908d8ffc38ba48b92
Jon Marien <nuclearsmilz@gmail.com>
2022-01-03 02:17:10 -0500

    fixed/added BaseLootTableProvider

---
+++ src/main/java/com/chrono/oneofzero/datagen/BaseLootTableProvider.java
@@ -0,0 +1,12 @@
+package com.chrono.oneofzero.datagen;
+
+import net.minecraft.data.DataGenerator;
+
+public class BaseLootTableProvider {
+	public BaseLootTableProvider (DataGenerator generatorIn) {
+	}
+	
+	protected void addTables(){
+	
+	}
+}
